cb2f07ca465358c1ed6fbb7d2be0e6a5f6f0499d
Stanislas DOLCINI <stanislas.dolcini@devinci.fr>
2017-12-11 17:48:32 +0100

    New library based on the IP scan for words

---
+++ FullScreen-Version/MediaFrameQrProcessing/Processors/WordFrameProcessor.cs
@@ -0,0 +1,74 @@
+﻿using MediaFrameQrProcessing.VideoDeviceFinders;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture.Frames;
+using Windows.Media.Ocr;
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Media.Capture;
+
+namespace MediaFrameQrProcessing.Processors
+{
+    public class WordFrameProcessor : MediaCaptureFrameProcessor
+    {
+        public string Result { get; private set; }
+        private string m_RequestWord;
+        private OcrEngine m_OcrEngine;
+
+        public WordFrameProcessor(string requestWord, MediaFrameSourceFinder mediaFrameSourceFinder, DeviceInformation videoDeviceInformation, string mediaEncodingSubtype, MediaCaptureMemoryPreference memoryPreference = MediaCaptureMemoryPreference.Cpu)
+        : base(mediaFrameSourceFinder, videoDeviceInformation, mediaEncodingSubtype, memoryPreference)
+        {
+            this.m_RequestWord = requestWord;
+        }
+
+        protected override async Task<bool> ProcessFrameAsync(MediaFrameReference frameReference)
+        {
+
+            // doc here https://msdn.microsoft.com/en-us/library/windows/apps/xaml/windows.media.capture.frames.videomediaframe.aspx
+            // says to dispose this softwarebitmap if you access it.
+            using (SoftwareBitmap bitmap = frameReference.VideoMediaFrame.SoftwareBitmap)
+            {
+                try
+                {
+                    if (this.m_OcrEngine == null)
+                        this.m_OcrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
+
+                    OcrResult ocrResult = await this.m_OcrEngine.RecognizeAsync(bitmap);
+
+                    if (ocrResult == null)
+                        return false;
+
+                    IEnumerable<string> matchingWordsPlural = FindSimilarWords(ocrResult.Text, m_RequestWord + 's');
+
+                    if (!ocrResult.Text.Contains(m_RequestWord) 
+                        && !ocrResult.Text.Contains(m_RequestWord + 's') && matchingWordsPlural.Count() == 0)
+                        return false;
+
+                    this.Result = Regex.Matches(ocrResult.Text, m_RequestWord).Count.ToString() + " exact match(es) and " + matchingWordsPlural.Count() + " approximate matches found";
+                    return true;
+                }
+                catch
+                {
+                }
+            }
+            return false;
+        }
+
+        private IEnumerable<string> FindSimilarWords(string entry, string requestWord)
+        {
+            IEnumerable<string> words = entry.Split(' ').ToList();
+
+            //Pick only the words with the same length.
+            words = words.Where(w => w.Length == requestWord.Length);
+
+            // Can't use the distance calculation if the word is too short
+            if(requestWord.Length > 4)
+                words = words.Where(w => LevenshteinDistance.Compute(w, requestWord) <= 2);
+
+            return words;
+        }
+    }
+}
---
+++ FullScreen-Version/MediaFrameQrProcessing/Wrappers/WordScanner.cs
@@ -0,0 +1,55 @@
+﻿using MediaFrameQrProcessing.Processors;
+using MediaFrameQrProcessing.VideoDeviceFinders;
+using System;
+using Windows.Devices.Enumeration;
+using Windows.Media.MediaProperties;
+
+namespace MediaFrameQrProcessing.Wrappers
+{
+    public static class WordScanner
+    {
+        public static WordFrameProcessor m_FrameProcessor;
+
+        public static async void ScanFirstCameraForWords(Action<string> resultCallback, TimeSpan timeout, string requestWord)
+        {
+            string result = string.Empty;
+
+            // Note - I keep this frame processor around which means keeping the
+            // underlying MediaCapture around because when I didn't keep it
+            // around I ended up with a crash in Windows.Media.dll related
+            // to disposing of the MediaCapture.
+            // So...this isn't what I wanted but it seems to work better :-(
+            if (m_FrameProcessor == null)
+            {
+                MediaFrameSourceFinder mediaFrameSourceFinder = new MediaFrameSourceFinder();
+                // We want a source of media frame groups which contains a color video
+                // preview (and we'll take the first one).
+                var populated = await mediaFrameSourceFinder.PopulateAsync(MediaFrameSourceFinder.ColorVideoPreviewFilter, MediaFrameSourceFinder.FirstOrDefault);
+
+                if (populated)
+                {
+                    // We'll take the first video capture device.
+                    DeviceInformation videoCaptureDevice = await VideoCaptureDeviceFinder.FindFirstOrDefaultAsync();
+
+                    if (videoCaptureDevice != null)
+                    {
+                        // Make a processor which will pull frames from the camera and run
+                        m_FrameProcessor = new WordFrameProcessor(requestWord, mediaFrameSourceFinder, videoCaptureDevice, MediaEncodingSubtypes.Bgra8);
+                        // Remember to ask for auto-focus on the video capture device.
+                        m_FrameProcessor.SetVideoDeviceControllerInitialiser(vd => vd.Focus.TrySetAuto(true));
+                    }
+                }
+            }
+            if (m_FrameProcessor != null)
+            {
+                // Process frames for up to 30 seconds to see if we get any words...
+                await m_FrameProcessor.ProcessFramesAsync(timeout);
+
+                // See what result we got.
+                result = m_FrameProcessor.Result;
+            }
+            // Call back with whatever result we got.
+            resultCallback(result);
+        }
+    }
+}
