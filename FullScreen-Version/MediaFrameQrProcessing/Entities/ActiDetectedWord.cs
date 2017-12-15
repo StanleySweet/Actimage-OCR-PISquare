f73b5e67f952933fb88bf2752e9b4d9e2a35b24d
Stanislas DOLCINI <stanislas.dolcini@devinci.fr>
2017-12-15 07:43:50 +0100

    Add missing files

---
+++ FullScreen-Version/MediaFrameQrProcessing/Entities/ActiDetectedWord.cs
@@ -0,0 +1,22 @@
+﻿namespace MediaFrameQrProcessing.Entities
+{
+    public class ActiDetectedWord
+    {
+        private double _posX;
+        private double _posY;
+        private double _width;
+        private double _height;
+        private bool _exactMatch;
+
+        public ActiDetectedWord(string originalText, double posX, double posY, double width, double height, bool exactMatch)
+        {
+            _posX = posX;
+            _posY = posY;
+            _height = height;
+            _width = width;
+            _exactMatch = exactMatch;
+        }
+        public bool IsExactMatch() => _exactMatch;
+
+    }
+}
