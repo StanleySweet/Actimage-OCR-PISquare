206cf3844ffc02d5aedbbccb4a15ef85f70fa617
Stanislas DOLCINI <stanislas.dolcini@devinci.fr>
2017-12-18 14:30:08 +0100

    Some more changes

--- FullScreen-Version/MediaFrameQrProcessing/VideoDeviceFinders/MediaFrameSourceFinder.cs
+++ FullScreen-Version/MediaFrameQrProcessing/VideoDeviceFinders/MediaFrameSourceFinder.cs
@@ -1,46 +1,48 @@
 ﻿namespace MediaFrameQrProcessing.VideoDeviceFinders
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Threading.Tasks;
-  using Windows.Media.Capture;
-  using Windows.Media.Capture.Frames;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Windows.Media.Capture;
+    using Windows.Media.Capture.Frames;
 
-  public class MediaFrameSourceFinder
-  {
-    public MediaFrameSourceGroup FrameSourceGroup { get; private set; }
-    public MediaFrameSourceInfo FrameSourceInfo { get; private set; }
-    public MediaFrameSourceFinder()
+    public class MediaFrameSourceFinder
     {
+        #region Properties
+        public MediaFrameSourceGroup FrameSourceGroup { get; private set; }
+        public MediaFrameSourceInfo FrameSourceInfo { get; private set; }
+        #endregion
+        #region Constructors
+        public MediaFrameSourceFinder()
+        {
 
+        }
+        #endregion
+        public async Task<bool> PopulateAsync(Func<MediaFrameSourceInfo, bool> sourceInfoFilter,
+            Func<IEnumerable<MediaFrameSourceGroup>, MediaFrameSourceGroup> sourceGroupSelector)
+        {
+            var mediaFrameSourceGroups = await MediaFrameSourceGroup.FindAllAsync();
+
+            var candidates = mediaFrameSourceGroups.Where(
+              group => group.SourceInfos.Any(sourceInfoFilter));
+
+            this.FrameSourceGroup = sourceGroupSelector(candidates);
+
+            this.FrameSourceInfo = this.FrameSourceGroup?.SourceInfos.FirstOrDefault(
+              sourceInfoFilter);
+
+            return ((this.FrameSourceGroup != null) && (this.FrameSourceInfo != null));
+        }
+        public static MediaFrameSourceGroup FirstOrDefault(
+          IEnumerable<MediaFrameSourceGroup> groups)
+        {
+            return (groups.FirstOrDefault());
+        }
+        public static bool ColorVideoPreviewFilter(MediaFrameSourceInfo sourceInfo)
+        {
+            return MediaStreamType.VideoRecord.Equals(sourceInfo.MediaStreamType)
+                      && MediaFrameSourceKind.Color.Equals(sourceInfo.SourceKind);
+        }
     }
-    public async Task<bool> PopulateAsync(
-      Func<MediaFrameSourceInfo, bool> sourceInfoFilter,
-      Func<IEnumerable<MediaFrameSourceGroup>, MediaFrameSourceGroup> sourceGroupSelector)
-    {
-      var mediaFrameSourceGroups = await MediaFrameSourceGroup.FindAllAsync();
-
-      var candidates = mediaFrameSourceGroups.Where(
-        group => group.SourceInfos.Any(sourceInfoFilter));
-
-      this.FrameSourceGroup = sourceGroupSelector(candidates);
-
-      this.FrameSourceInfo = this.FrameSourceGroup?.SourceInfos.FirstOrDefault(
-        sourceInfoFilter);
-
-      return ((this.FrameSourceGroup != null) && (this.FrameSourceInfo != null));
-    }
-    public static MediaFrameSourceGroup FirstOrDefault(
-      IEnumerable<MediaFrameSourceGroup> groups)
-    {
-      return (groups.FirstOrDefault());
-    }
-    public static bool ColorVideoPreviewFilter(MediaFrameSourceInfo sourceInfo)
-    {
-      return (
-        (sourceInfo.MediaStreamType == MediaStreamType.VideoRecord) &&
-        (sourceInfo.SourceKind == MediaFrameSourceKind.Color));
-    }
-  }
 }
