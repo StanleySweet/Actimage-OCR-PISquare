c3fd8abfb041eab91a7fec7c4d5604329e2ffe56
Stanislas DOLCINI <stanislas.dolcini@devinci.fr>
2017-12-18 13:13:17 +0100

    Remove debug inside the camera preview

--- FullScreen-Version/UnityProject/Assets/DisplayWebcam.cs
+++ FullScreen-Version/UnityProject/Assets/DisplayWebcam.cs
@@ -10,15 +10,7 @@ namespace Assets
         void Start()
         {
             WebCamDevice[] devices = WebCamTexture.devices;
-
-            // for debugging purposes, prints available devices to the console
-            for (int i = 0; i < devices.Length; i++)
-            {
-                print("Webcam available: " + devices[i].name);
-            }
-
             Renderer rend = this.GetComponentInChildren<Renderer>();
-
             // assuming the first available WebCam is desired
             WebCamTexture tex = new WebCamTexture(devices[0].name);
             rend.material.mainTexture = tex;
